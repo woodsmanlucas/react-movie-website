a2bedd10ddc757521f674200172b8f694c482148
Lucas <woodsman.lucas@gmail.com>
2019-12-25 16:14:03 -0800

    added bootstrap and made a card on the movie page

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import '../styles/bootstrap.min.css'
 import './App.css'
 import Moment from 'moment'
 
@@ -13,30 +14,37 @@ export function Movie(props){
       async function getMovie(id){
           const data = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=47c4adc75b16f23db3cf78e4870a4296&language=en-US`)
           data.json().then(function(value) {
-                setMovie(value); // <- your promise data exists here.
-             })
+                setMovie(value); 
+                console.log(value)
+            })
             
         }
 
 
     return (
-        <div className="wrapper"  style = {{backgroundImage: `url(https://image.tmdb.org/t/p/w500${movie.poster_path})`,
-                                           backgroundRepeat : "no-repeat",
-                                           backgroundPosition: "center",
-                                           backgroundSize : "cover"}}>
-            <div className="header">
-                <div className="date">
-                    <span>{Moment(movie.release_date).format('MMM d YYYY')}</span>
+        <div className="container">
+        <div className="row">
+            <div className="col align-self-center">
+        <div className="card movie"> 
+            <img class="card-img-top" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>
+            <div className="card-body">
+                <div className="row date info">
+                    <span class="col">Release Date: {Moment(movie.release_date).format('MMM d YYYY')}</span>
+                    <span class="col">Budget: {movie.budget}</span>
+                    <span class="col">{movie.tagline}</span>
                 </div>
                 <ul className="menu-content">
                     <li><a href="#" className="fa fa-heart-o"><span>{movie.vote_average}</span></a></li>
                 </ul>
             </div>
-            <div className="data">
+            <div className="card-text">
                 <div className="content">
                     <p className="text">{movie.overview}</p>
                 </div>
              </div>
+        </div>
+        </div>
+        </div>
         </div>
     )
 }
