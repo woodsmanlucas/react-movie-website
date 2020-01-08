17497a872979db49d0239c06d184427b3a1ba155
Lucas <woodsman.lucas@gmail.com>
2020-01-07 16:21:01 -0800

    fixed styles on mobile and updated readme

--- src/components/SearchMovie.jsx
+++ src/components/SearchMovie.jsx
@@ -30,7 +30,7 @@ function SearchMovie(props){
     }
 
     return(
-        <form className="form-inline" onSubmit={handleSubmit}>
+        <form className="form-inline flex-nowrap" onSubmit={handleSubmit}>
             <input id="input" className="form-control mr-sm-2" type="search" placeholder="Search Movie" 
             aria-label="Search" name="search" value={query} onChange={handleQuery}/>
             <button className="btn btn-outline-success my-2 my-sm-0" type="submit" value="submit" >Search</button>
